3b0a8f7b5de39a9ac7f912e4dd6b31faec87a55d
Alanto Manu <alantomanu501@gmail.com>
2024-12-05 14:51:09 +0530

    created status for orders in orders model

--- nexus_trove/orders/models.py
+++ nexus_trove/orders/models.py
@@ -5,7 +5,15 @@ from customers.models import Customer
 class Order(models.Model):
     LIVE=1
     DELETE=0
+    CART_STAGE=1
     DELETE_CHOICES=((LIVE,'Live'),(DELETE,'Delete'))
+    ORDER_CONFIRMED=1
+    ORDER_PROCESSED=2
+    ORDER_DELIVERD=3
+    ORDER_REJECTED=4
+    STATUS_CHOICE=((ORDER_PROCESSED,"ORDER_PROCESSED"),
+                   (ORDER_DELIVERD,"ORDER_DELIVERD")
+                   (ORDER_REJECTED,"ORDER_REJECTED"))
     owner=models.ForeignKey(Customer,on_delete=models.SET_NULL,related_name='orders')
     delete_status=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
     created_at=models.DateTimeField(auto_now_add=True)
