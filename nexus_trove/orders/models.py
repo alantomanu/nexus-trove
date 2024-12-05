eacf6120d7e9a3616732d10b8a537c5dd9daa642
Alanto Manu <alantomanu501@gmail.com>
2024-12-05 14:41:01 +0530

    added new class in orders as orderd item

--- nexus_trove/orders/models.py
+++ nexus_trove/orders/models.py
@@ -10,6 +10,7 @@ class Order(models.Model):
     delete_status=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
-class CartItem(models.Model):
+class OrderdItem(models.Model):
     product=models.ForeignKey(Product,related_name='added_carts',on_delete=models.SET_NULL)
     quantity=models.IntegerField(default=1)
+    owner=models.ForeignKey(Order,on_delete=models.CASCADE,related_name='added_items')
