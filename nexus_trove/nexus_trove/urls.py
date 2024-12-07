dfd7eb34681d590cc8c43d44a33d0f074ff9c54a
Alanto Manu <alantomanu501@gmail.com>
2024-12-07 17:51:01 +0530

    changed the location of the templates

--- nexus_trove/nexus_trove/urls.py
+++ nexus_trove/nexus_trove/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('products.url'))
+    path('', include('products.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
