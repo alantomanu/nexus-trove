4a65ea57e208d16319e2900b497f0d293ddf1f7e
Alanto Manu <alantomanu501@gmail.com>
2024-12-08 23:13:05 +0530

    corrected some errors in models

--- nexus_trove/orders/models.py
+++ nexus_trove/orders/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from products.models import Product
 from customers.models import Customer
 #data model for order
+# The Order model represents a customer's order in the system. 
+# It includes fields to track the order's status, the customer who owns the order, 
+# and timestamps for when the order was created and last updated. 
+# The order_status field uses predefined choices to indicate the current state of the order, 
+# while delete_status indicates whether the order is live or marked for deletion.
+
+# The OrderdItem model represents individual items within an order. 
+# Each item is linked to a specific product and includes a quantity field to specify 
+# how many of that product are included in the order. 
+# The owner field establishes a relationship with the Order model, 
+# allowing multiple items to be associated with a single order.
 class Order(models.Model):
     LIVE=1
     DELETE=0
@@ -15,7 +26,7 @@ class Order(models.Model):
                    (ORDER_DELIVERD,"ORDER_DELIVERD"),
                    (ORDER_REJECTED,"ORDER_REJECTED"))
     order_status=models.IntegerField(choices=STATUS_CHOICE,default=CART_STAGE)
-    owner=models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True,related_name='orders')
+    owner=models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True,related_name='orders')#foreignkey indicate one to  many
     delete_status=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
