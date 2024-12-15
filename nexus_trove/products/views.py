541f3451b921de154f2f3d49254def5b9d3d5bca
Alanto Manu <alantomanu501@gmail.com>
2024-12-15 23:27:53 +0530

    crated new template products_laout.html

--- nexus_trove/products/views.py
+++ nexus_trove/products/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 # Create your views here.
 def index(request):
-    return render(request,'blank_layout.html')
+    return render(request,'index.html')
+def list_products(request):
+    return render(request,'products_layout.html')
