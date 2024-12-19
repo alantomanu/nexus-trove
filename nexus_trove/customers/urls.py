88c6cc3aef9935f150fc32a36dbc101dfefa13b0
Alanto Manu <alantomanu501@gmail.com>
2024-12-19 23:09:40 +0530

    added acccounts and account _banner template and necessary details

--- nexus_trove/customers/urls.py
+++ nexus_trove/customers/urls.py
@@ -5,5 +5,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 urlpatterns = [
-    path('account',views.account,name='account'),
+    path('account',views.show_account,name='account'),
 ]
