9117be08c7ae5a1379269f8a8e41a2686943c2d9
Alanto Manu <alantomanu501@gmail.com>
2024-12-21 00:47:38 +0530

    wrote function to retrive products added via admin

--- nexus_trove/products/views.py
+++ nexus_trove/products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from . models import Product
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -11,6 +12,7 @@ def list_products(request):
     Returns:
         _type_: _description_
     """
+    product_list=Product.objects.all()
     return render(request,'products.html')
 def detail_product(request):
     return render(request,'product_detail.html')
