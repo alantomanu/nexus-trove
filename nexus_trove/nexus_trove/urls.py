a7bca766a941d3d5d2ec5dca39a8d664ba984b85
Alanto Manu <alantomanu501@gmail.com>
2024-12-20 23:06:04 +0530

    added new file cart.html and necessary things to it

--- nexus_trove/nexus_trove/urls.py
+++ nexus_trove/nexus_trove/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
     path('customer', include('customers.urls')),
+    path('orders', include('orders.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
